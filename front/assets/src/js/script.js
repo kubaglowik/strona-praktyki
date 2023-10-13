2a92bdced4d5fc9c2e5287a1dc8e77e1250dcf4b
kubaglowik <jakubgo45@gmail.com>
2023-10-13 13:57:13 +0200

    change name in slider.js

--- front/assets/src/js/script.js
+++ front/assets/src/js/script.js
@@ -2,7 +2,7 @@
  * SASS
  */
 import '../sass/layout.scss';
-
+import { slider } from './modules/your_slider_module_name.js'
 /**
  * JavaScript
  */
@@ -11,23 +11,3 @@ import '../sass/layout.scss';
  * Add here your JavasScript code
  */
 
-  let i = 0;
-  let images = [];
-  let time = 1000;
-
-  let url = window.location.href;
-
-  images[0] = url + 'app/themes/kuba/dist/images/a.jpg';
-  images[1] = url + 'app/themes/kuba/dist/images/b.jpg';
-  images[2] = url + 'app/themes/kuba/dist/images/c.jpg';
-
-  function changeimg() {
-    document.slide.src = images[i];
-  if(i < images.lenght - 1){
-    i++;
-  }else{
-    i = 0;
-  }
-    setTimeout("changeimg()",time);
-  }
-  window.onload = changeimg;
