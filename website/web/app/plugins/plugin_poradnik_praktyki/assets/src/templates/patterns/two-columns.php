667e4f0feb23dbbef82c573e092dfe3a444e612c
kubaglowik <jakubgo45@gmail.com>
2023-10-25 16:02:20 +0200

    added other wp style

--- website/web/app/plugins/plugin_poradnik_praktyki/assets/src/templates/patterns/two-columns.php
+++ website/web/app/plugins/plugin_poradnik_praktyki/assets/src/templates/patterns/two-columns.php
@@ -7,48 +7,50 @@
 
 ?>
 
-<!-- wp:columns -->
+<!-- wp:group -->
+<div class="wp-block-group"><!-- wp:columns -->
 <div class="wp-block-columns"><!-- wp:column -->
-	<div class="wp-block-column"><!-- wp:group {"backgroundColor":"black","textColor":"white"} -->
-		<div class="wp-block-group has-white-color has-black-background-color has-text-color has-background"><!-- wp:heading {"textAlign":"center","textColor":"pale-cyan-blue"} -->
-			<h2 class="has-text-align-center has-pale-cyan-blue-color has-text-color">Heading One</h2>
-			<!-- /wp:heading -->
+<div class="wp-block-column"><!-- wp:group {"backgroundColor":"black","textColor":"white"} -->
+<div class="wp-block-group has-white-color has-black-background-color has-text-color has-background"><!-- wp:heading {"textAlign":"center","level":1,"textColor":"pale-cyan-blue"} -->
+<h1 class="has-text-align-center has-pale-cyan-blue-color has-text-color">Heading One</h1>
+<!-- /wp:heading -->
 
-			<!-- wp:paragraph {"align":"center","textColor":"cyan-bluish-gray"} -->
-			<p class="has-text-align-center has-cyan-bluish-gray-color has-text-color">Description</p>
-			<!-- /wp:paragraph -->
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">descirption</p>
+<!-- /wp:paragraph -->
 
-			<!-- wp:paragraph {"align":"center"} -->
-			<p class="has-text-align-center"><strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
-			<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p><strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+<!-- /wp:paragraph -->
 
-			<!-- wp:buttons -->
-			<div class="wp-block-buttons"><!-- wp:button {"align":"center","className":"is-style-layout-border-white-no-fill"} -->
-				<div class="wp-block-button aligncenter is-style-layout-border-white-no-fill"><a class="wp-block-button__link wp-element-button">CTA One</a></div>
-				<!-- /wp:button --></div>
-			<!-- /wp:buttons --></div>
-		<!-- /wp:group --></div>
-	<!-- /wp:column -->
+<!-- wp:buttons {"contentJustification":"center"} -->
+<div class="wp-block-buttons is-content-justification-center"><!-- wp:button {"className":"is-style-outline"} /--></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
 
-	<!-- wp:column -->
-	<div class="wp-block-column"><!-- wp:group {"backgroundColor":"black","textColor":"white"} -->
-		<div class="wp-block-group has-white-color has-black-background-color has-text-color has-background"><!-- wp:heading {"textAlign":"center","textColor":"pale-cyan-blue"} -->
-			<h2 class="has-text-align-center has-pale-cyan-blue-color has-text-color">Heading Two</h2>
-			<!-- /wp:heading -->
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"backgroundColor":"black","textColor":"white"} -->
+<div class="wp-block-group has-white-color has-black-background-color has-text-color has-background"><!-- wp:heading {"textAlign":"center","level":1,"textColor":"pale-cyan-blue"} -->
+<h1 class="has-text-align-center has-pale-cyan-blue-color has-text-color">Heading Two</h1>
+<!-- /wp:heading -->
 
-			<!-- wp:paragraph {"align":"center","textColor":"cyan-bluish-gray"} -->
-			<p class="has-text-align-center has-cyan-bluish-gray-color has-text-color">Description</p>
-			<!-- /wp:paragraph -->
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">descirption</p>
+<!-- /wp:paragraph -->
 
-			<!-- wp:paragraph {"align":"center"} -->
-			<p class="has-text-align-center">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors.</p>
-			<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p><strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+<!-- /wp:paragraph -->
 
-			<!-- wp:buttons -->
-			<div class="wp-block-buttons"><!-- wp:button {"align":"center","className":"is-style-layout-border-white-no-fill"} -->
-				<div class="wp-block-button aligncenter is-style-layout-border-white-no-fill"><a class="wp-block-button__link wp-element-button">CTA Two</a></div>
-				<!-- /wp:button --></div>
-			<!-- /wp:buttons --></div>
-		<!-- /wp:group --></div>
-	<!-- /wp:column --></div>
-<!-- /wp:columns -->
+<!-- wp:buttons {"contentJustification":"center"} -->
+<div class="wp-block-buttons is-content-justification-center"><!-- wp:button {"className":"is-style-outline"} /--></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group -->
+
+<!-- wp:group -->
+<div class="wp-block-group"></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
