bff4e5e44ba32aea009a3c59d4802d6d2f73da3b
kubaglowik <jakubgo45@gmail.com>
2023-10-26 12:45:18 +0200

    delete var dumps

--- website/web/app/plugins/plugin_poradnik_praktyki/src/Plugin.php
+++ website/web/app/plugins/plugin_poradnik_praktyki/src/Plugin.php
@@ -27,7 +27,6 @@ class Plugin {
 	 * Initialize plugin
 	 */
 	private function init() {
-		// var_dump("klasa plugin dziala");
 		define( 'AQUILA_FEATURES_PLUGIN_PATH', untrailingslashit( plugin_dir_path( __DIR__ ) ) );
 		define( 'AQUILA_FEATURES_PLUGIN_URL', untrailingslashit( plugin_dir_url( __DIR__ ) ) );
 		define( 'AQUILA_FEATURES_PLUGIN_BUILD_PATH', AQUILA_FEATURES_PLUGIN_PATH . '/assets/src/build' );
@@ -35,7 +34,6 @@ class Plugin {
 		define( 'AQUILA_FEATURES_PLUGIN_VERSION', '1.0.0' );
 
 		new Assets();
-		// var_dump("klasa assets sie wykonala");
 		new Patterns();
 		// new SearchApi();
 	}
