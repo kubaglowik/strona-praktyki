e0281f402c3c79e3ae27eb6f6ddcf5bce1008eaf
kubaglowik <jakubgo45@gmail.com>
2023-10-26 15:35:29 +0200

    slider fix

--- front/assets/src/js/modules/slider.js
+++ front/assets/src/js/modules/slider.js
@@ -1,24 +1,39 @@
 export function slider() {
-  let i = 0;
+  // let i = 0;
   let images = [];
-  let time = 1000;
-
+  // let time = 1000;
   let url = window.location.href;
-
   images[0] = url + 'app/themes/kuba/dist/images/a.jpg';
   images[1] = url + 'app/themes/kuba/dist/images/b.jpg';
   images[2] = url + 'app/themes/kuba/dist/images/c.jpg';
 
+
+  let currentSlide = 0;
+  document.slide.src = images[currentSlide];
+
+  const prev_btn = document.querySelector('.prev');
+  const next_btn = document.querySelector('.next');
+
+  prev_btn.addEventListener("click", prevSlide)
+  next_btn.addEventListener("click", nextSlide)
+
+  function prevSlide() {
+    currentSlide--;
+    changeImg()
+  }
+
+  function nextSlide() {
+    currentSlide++;
+    changeImg()
+  }
+
   function changeImg() {
-    document.slide.src = images[i];
-    if (i < images.lenght - 1) {
-      i++;
-    } else {
-      i = 0;
+    if (currentSlide > 0 && currentSlide < images.length) {
+      document.slide.src = images[currentSlide];
+    }
+    else {
+      currentSlide = 0;
     }
   }
-  
-  window.addEventListener("load", function () {
-    setTimeout(changeImg(), time);
-  });
+
 }
